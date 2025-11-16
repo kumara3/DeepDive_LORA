8a549b03bca26d8658c2fbcaff06db913e83406a
kumara3 <ashwani.vit@gmail.com>
2025-11-16 09:12:51 -0600

    function swap from preprocess to model setup

--- Model_setup.py
+++ Model_setup.py
@@ -147,6 +147,44 @@ class GPT(nn.Module):
         logits = self.out_head(x)
         return logits
 
+def assign_check(target, source):
+    if target.shape != source.T.shape:
+        raise ValueError(f"Shape mismatch: target {target.shape}, source {source.shape}")
+    return torch.nn.Parameter(source.clone().detach())
+
+def load_weights(gpt, gpt_hf, BASE_CONFIG):
+    d = gpt_hf.state_dict()
+    gpt.position_embedding.weight = assign_check(gpt.position_embedding.weight, d["wpe.weight"])
+    gpt.token_embedding.weight = assign_check(gpt.token_embedding.weight, d["wte.weight"]) 
+    for b in range(BASE_CONFIG["n_layers"]):
+        q_w, k_w, v_w = np.split(d[f"h.{b}.attn.c_attn.weight"], 3, axis=-1)
+        gpt.transformer_blocks[b].att.W_q.weight = assign_check(gpt.transformer_blocks[b].att.W_q.weight, q_w.T)
+        gpt.transformer_blocks[b].att.W_k.weight = assign_check(gpt.transformer_blocks[b].att.W_k.weight, k_w.T)
+        gpt.transformer_blocks[b].att.W_v.weight = assign_check(gpt.transformer_blocks[b].att.W_v.weight, v_w.T)
+
+        q_b, k_b, v_b = np.split(d[f"h.{b}.attn.c_attn.bias"], 3, axis=-1)
+        gpt.transformer_blocks[b].att.W_q.bias = assign_check(gpt.transformer_blocks[b].att.W_q.bias, q_b)
+        gpt.transformer_blocks[b].att.W_k.bias = assign_check(gpt.transformer_blocks[b].att.W_k.bias, k_b)
+        gpt.transformer_blocks[b].att.W_v.bias = assign_check(gpt.transformer_blocks[b].att.W_v.bias, v_b)
+
+
+        gpt.transformer_blocks[b].att.out.weight = assign_check(gpt.transformer_blocks[b].att.out.weight, d[f"h.{b}.attn.c_proj.weight"].T)
+        gpt.transformer_blocks[b].att.out.bias = assign_check(gpt.transformer_blocks[b].att.out.bias, d[f"h.{b}.attn.c_proj.bias"])
+
+        gpt.transformer_blocks[b].ff.layers[0].weight = assign_check(gpt.transformer_blocks[b].ff.layers[0].weight, d[f"h.{b}.mlp.c_fc.weight"].T)
+        gpt.transformer_blocks[b].ff.layers[0].bias = assign_check(gpt.transformer_blocks[b].ff.layers[0].bias, d[f"h.{b}.mlp.c_fc.bias"])
+        gpt.transformer_blocks[b].ff.layers[2].weight = assign_check(gpt.transformer_blocks[b].ff.layers[2].weight, d[f"h.{b}.mlp.c_proj.weight"].T)
+        gpt.transformer_blocks[b].ff.layers[2].bias = assign_check(gpt.transformer_blocks[b].ff.layers[2].bias, d[f"h.{b}.mlp.c_proj.bias"])
+
+        gpt.transformer_blocks[b].norm1.scale = assign_check(gpt.transformer_blocks[b].norm1.scale, d[f"h.{b}.ln_1.weight"])
+        gpt.transformer_blocks[b].norm1.shift = assign_check(gpt.transformer_blocks[b].norm1.shift, d[f"h.{b}.ln_1.bias"])
+        gpt.transformer_blocks[b].norm2.scale = assign_check(gpt.transformer_blocks[b].norm2.scale, d[f"h.{b}.ln_2.weight"])
+        gpt.transformer_blocks[b].norm2.shift = assign_check(gpt.transformer_blocks[b].norm2.shift, d[f"h.{b}.ln_2.bias"])
+
+        gpt.final_norm.scale = assign_check(gpt.final_norm.scale, d["ln_f.weight"])
+        gpt.final_norm.shift = assign_check(gpt.final_norm.shift, d["ln_f.bias"])
+        gpt.out_head.weight = assign_check(gpt.out_head.weight, d["wte.weight"])
+
 def calc_accuracy(data_loader, model, num_batches=None):
     model.eval()
     correct_predictions, num_examples = 0, 0
